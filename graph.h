ff119a7d98df4c7ce9070fda2f30d1cc43738efb
Niranjan <73375333+niranjanreji@users.noreply.github.com>
2023-11-13 17:07:12 -0500

    .

---
+++ graph.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <vector>
+
+template <class D, class K>
+class Graph {
+    private:
+
+    public:
+        Graph(vector<K> keys, vector<D> data, vector<vector<K>> edges);
+};
