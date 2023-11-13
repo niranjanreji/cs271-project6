82fd92d824d2822a64e6d52b548878e8573be0b1
Niranjan <73375333+niranjanreji@users.noreply.github.com>
2023-11-13 17:23:11 -0500

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,10 +1,21 @@
 #pragma once
 #include <vector>
+#include <string>
 
 template <class D, class K>
 class Graph {
     private:
-
+        struct Vertex {
+            D data;
+            K key;
+            vector<K> adj;
+        };
     public:
         Graph(vector<K> keys, vector<D> data, vector<vector<K>> edges);
+        Vertex* get(K key);
+        bool reachable(K start, K end);
+        void bfs(K start);
+        void print_path(K start, K end);
+        string edge_class(K start, K end);
+        void bfs_tree(K start);
 };
